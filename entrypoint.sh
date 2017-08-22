34aa0fa6cfc62d7e2b01b795535aa13f9c94e272
msteven101 <mwsteven@odu.edu>
2017-08-22 19:13:22 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 useradd -u $CLOUD9_UID $CLOUD9_USER
 
-sed -i -e 's@John Doe@'"$CLOUD9_DN"'@' /cloud9/plugins/c9.vfs.standalone/standalone.js
-sed -i -e 's@root@'"$CLOUD9_USER"'@' /etc/supervisor/conf.d/cloud9.conf
+sed -i -e "s/John Doe/$CLOUD9_DN/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
+sed -i -e "s/johndoe/$CLOUD9_USER/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
+
+sed -i -e "s/root/$CLOUD9_USER/g" /etc/supervisor/conf.d/cloud9.conf
 
 su $CLOUD9_USER
