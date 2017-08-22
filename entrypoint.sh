54126aeda15a45e9ada82809daa3554fcc5affe1
msteven101 <mwsteven@odu.edu>
2017-08-22 02:31:13 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-useradd -u $CLOUD9_UID -g $CLOUD9_GID $CLOUD9_USER
+useradd -u $CLOUD9_UID $CLOUD9_USER
 
 sed -i -e 's/John Doe/$CLOUD9_DN/g' /cloud9/settings/standalone.js
 sed -i -e 's/johndoe/$CLOUD9_USER/g' /cloud9/settings/standalone.js
