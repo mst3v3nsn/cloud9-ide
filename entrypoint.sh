0e20f2861a6e912e2eb301129bff0e9bab4fca20
msteven101 <mwsteven@odu.edu>
2017-08-23 15:21:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sed -i -e "s/John Doe/$CLOUD9_DN/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
 sed -i -e "s/johndoe/$CLOUD9_USER/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
 sed -i -e "s/example.org/cs.odu.edu/g" /cloud9/plugins/c9.vfs.standalone/standalone.js 
