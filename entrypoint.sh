ac887d5845e75d9595aab71ef539e860d8573766
msteven101 <mwsteven@odu.edu>
2017-08-23 00:55:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-useradd -u $CLOUD9_UID $CLOUD9_USER
-
-sed "s/John Doe/$CLOUD9_DN/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
-sed "s/johndoe/$CLOUD9_USER/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
-
-sed "s/root/$CLOUD9_USER/g" /etc/supervisor/conf.d/cloud9.conf
-
-su $CLOUD9_USER
+sed -i -e "s/John Doe/$CLOUD9_DN/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
+sed -i -e "s/johndoe/$CLOUD9_USER/g" /cloud9/plugins/c9.vfs.standalone/standalone.js
+sed -i -e "s/example.org/cs.odu.edu/g" /cloud9/plugins/c9.vfs.standalone/standalone.js 
+sed -i -e "s/root/$CLOUD9_USER/g" /etc/supervisor/conf.d/cloud9.conf
